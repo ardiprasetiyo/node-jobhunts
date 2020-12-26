2b040a78553e53739377205842704a56d2883e28
ardiprasetiyo <ardiprasetiyo@students.smkn3bandung.sch.id>
2020-12-26 11:50:19 +0700

    [ADD] Add Helmet Protection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors')
+const helmet = require('helmet')
 
 const APIRoutes = require('./routes/api')
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public/vue-jobhunts/dist')));
+app.use(helmet())
 // SSL
 app.use('/.well-known', express.static(path.join(__dirname, '.well-known')))
 app.use('/api', APIRoutes);
